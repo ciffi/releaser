74f47275d08223b3d8276d40381caf487619281b
ciffi <alessio@ciffi.it>
2024-03-15 17:03:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").exec;
+
+const rootPath = path.resolve(process.env.INIT_CWD || process.cwd());
+const huskyHiddenPath = path.resolve(rootPath, ".husky");
+const packageJsonFile = path.resolve(rootPath, "package.json");
+const packageJson = JSON.parse(fs.readFileSync(packageJsonFile, "utf8"));
+
+if (!fs.existsSync(huskyHiddenPath)) {
+  exec(`npx husky init`, { cwd: rootPath }, () => {
+    exec(`echo yarn commitlint --edit $1 > ${huskyHiddenPath}/commit-msg`, {
+      cwd: rootPath,
+    });
+    exec(`echo > ${huskyHiddenPath}/pre-commit`, { cwd: rootPath });
+    fs.writeFileSync(
+      packageJsonFile,
+      JSON.stringify(
+        {
+          ...packageJson,
+          scripts: {
+            ...packageJson.scripts,
+            version: "auto-changelog -p && git add CHANGELOG.md",
+          },
+          commitlint: {
+            extends: ["@commitlint/config-conventional"],
+          },
+          "auto-changelog": {
+            hideCredit: true,
+            template: "compact",
+          },
+        },
+        null,
+        2
+      ),
+      "utf8"
+    );
+  });
+} else {
+  console.log("husky init already done");
+}
+
